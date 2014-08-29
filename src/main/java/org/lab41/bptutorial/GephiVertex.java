687c4772132282c3a6f8bbdf99555f99a7351540
Erick Tryzelaar <erickt@lab41.org>
2014-08-28 17:01:26 -0700

    initial conversion to maven

---
+++ src/main/java/org/lab41/bptutorial/GephiVertex.java
@@ -0,0 +1,347 @@
+package org.lab41.bptutorial;
+
+/**
+ * Created by aganesh on 7/15/14.
+ */
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.VertexQuery;
+import com.tinkerpop.blueprints.util.DefaultVertexQuery;
+import com.tinkerpop.blueprints.util.MultiIterable;
+import com.tinkerpop.blueprints.util.StringFactory;
+import org.gephi.graph.api.Node;
+import org.gephi.graph.api.Edge;
+
+import java.util.*;
+
+
+public class GephiVertex extends GephiElement implements Vertex {
+
+    public GephiVertex(Node node, GephiGraph graph){
+        super(graph);
+        this.element = node;
+    }
+
+    public boolean equals(final Object object){
+        return object instanceof GephiVertex && ((GephiVertex) object).getId().equals(this.getId());
+    }
+
+    public Iterable<com.tinkerpop.blueprints.Edge> getEdges(Direction direction, String... labels){
+        if(direction.equals(Direction.OUT)){
+            return new GephiIncidentEdgeIterable(this.graph,(Node) this.element,"OUT",labels);
+        }
+        else if(direction.equals(Direction.IN)){
+            return new GephiIncidentEdgeIterable(this.graph,(Node) this.element,"IN",labels);
+        }
+        else{
+            return new MultiIterable(Arrays.asList(new GephiIncidentEdgeIterable(this.graph, (Node) this.element, "OUT", labels) , new GephiIncidentEdgeIterable(this.graph, (Node)this.element, "IN", labels))  );
+        }
+
+    }
+    public Node getRawVertex(){
+        return (Node)this.element;
+    }
+
+    public String toString(){
+        return StringFactory.vertexString(this);
+    }
+
+    public Iterable<Vertex> getVertices(Direction direction, String... labels){
+        if(direction.equals(Direction.OUT)){
+            return new GephiAdjacentVertexIterable(this.graph,(Node)this.element,"OUT",labels);
+        }
+        else if(direction.equals(Direction.IN)){
+            return new GephiAdjacentVertexIterable(this.graph,(Node)this.element,"IN",labels);
+        }
+        else{
+            return new MultiIterable(Arrays.asList(new GephiAdjacentVertexIterable(this.graph, (Node)this.element, "OUT", labels) , new GephiAdjacentVertexIterable(this.graph, (Node)this.element, "IN", labels))  );
+        }
+    }
+    public VertexQuery query(){
+        return new DefaultVertexQuery(this);
+    }
+
+    public com.tinkerpop.blueprints.Edge addEdge(String label, Vertex inVertex){
+        return this.graph.addEdge(null,this,inVertex,label);
+    }
+
+    private class GephiAdjacentVertexIterable<T extends com.tinkerpop.blueprints.Vertex> implements Iterable<GephiVertex>{
+        private GephiGraph graph;
+        private Node node;
+        private String direction;
+        private HashSet<String> labelHashSet;
+
+        public GephiAdjacentVertexIterable(GephiGraph graph, Node node, String direction,String... labels){
+            this.graph = graph;
+            this.node = node;
+            this.direction = direction;
+            labelHashSet = new HashSet<String>();
+            for(String x : labels) labelHashSet.add(x);
+        }
+
+        public Iterator<GephiVertex> iterator(){
+
+            //TODO Cut this down
+            final Iterator<Node> itIn = graph.getGraphModel().getDirectedGraph().getPredecessors(node).toCollection().iterator();
+            final Iterator<Node> itOut = graph.getGraphModel().getDirectedGraph().getSuccessors(node).toCollection().iterator();
+            final Iterator<Edge> itEdgeIn = graph.getGraphModel().getDirectedGraph().getInEdges(node).toCollection().iterator();
+            final Iterator<Edge> itEdgeOut = graph.getGraphModel().getDirectedGraph().getOutEdges(node).toCollection().iterator();
+
+            return new Iterator<GephiVertex>() {
+                Edge nextEdge = null;
+                @Override
+                public boolean hasNext() {
+
+                    if(labelHashSet.size() == 0){
+                        if(direction.equals("IN")){
+                            return itIn.hasNext();
+                        }
+                        else if(direction.equals("OUT")){
+                            return itOut.hasNext();
+                        }
+                    }
+
+                    if (nextEdge != null) {
+                        return true;
+                    }
+
+                    //Search for the next thing
+                    if (direction.equals("IN")) {
+                        while (itEdgeIn.hasNext()) {
+                            Edge temp = itEdgeIn.next();
+                                if (labelHashSet.contains(temp.getTypeLabel())) {
+                                    nextEdge = temp;
+                                    return true;
+                                }
+                        }
+
+                    } else {
+                        while (itEdgeOut.hasNext()) {
+
+                            Edge temp = itEdgeOut.next();
+                                if (labelHashSet.contains(temp.getTypeLabel())) {
+                                    nextEdge = temp;
+                                    return true;
+                                }
+                        }
+
+                    }
+                    return false;
+
+                }
+
+                @Override
+                public GephiVertex next() {
+                    if(labelHashSet.size() == 0){
+                        if(direction.equals("IN")){
+                            while(itIn.hasNext()) {
+                                return new GephiVertex(itIn.next(), graph);
+                            }
+                        }
+                        else if(direction.equals("OUT")){
+                            while(itOut.hasNext()) {
+                                return new GephiVertex(itOut.next(), graph);
+                            }
+                        }
+                    }
+
+                    if(nextEdge != null){
+                        Edge temp = nextEdge;
+                        nextEdge = null;
+
+                        //TODO Rethink this case
+                        if(temp.getSource().equals(node)) {
+                            return new GephiVertex(temp.getTarget(), graph);
+                        }
+                        else{
+                            return new GephiVertex(temp.getSource(), graph);
+                        }
+                    }
+
+                    //Search for the next thing
+                    if (direction.equals("IN")) {
+                        while (itEdgeIn.hasNext()) {
+                            Edge temp = itEdgeIn.next();
+                                if (labelHashSet.contains(temp.getTypeLabel())) {
+                                    return new GephiVertex(temp.getSource(), graph);
+                                }
+                        }
+
+                    } else {
+                        while (itEdgeOut.hasNext()) {
+                            Edge temp = itEdgeOut.next();
+                                if (labelHashSet.contains(temp.getTypeLabel())) {
+                                    return new GephiVertex(temp.getTarget(), graph);
+                                }
+                        }
+
+                    }
+
+                    throw new NoSuchElementException();
+
+                }
+
+                @Override
+                public void remove() {
+                    if (direction.equals("IN")){
+                        if(labelHashSet.size() == 0) {
+                            itIn.remove();
+                        }
+                        else {
+                            itEdgeIn.remove();
+                        }
+                    }
+                    else if(direction.equals("OUT")){
+                        if(labelHashSet.size() == 0){
+                            itOut.remove();
+                        }
+                        else {
+                            itEdgeOut.remove();
+                        }
+                    }
+                }
+            };
+        }
+
+
+    }
+
+
+    private class GephiIncidentEdgeIterable<T extends com.tinkerpop.blueprints.Edge> implements Iterable<GephiEdge>{
+        private GephiGraph graph;
+        private Node node;
+        private String direction;
+        private HashSet<String> labelHashSet;
+
+        public GephiIncidentEdgeIterable(GephiGraph graph, Node node, String direction,String... labels){
+            this.graph = graph;
+            this.node = node;
+            this.direction = direction;
+            labelHashSet = new HashSet<String>();
+            for(String i:labels) labelHashSet.add(i);
+        }
+
+        public Iterator<GephiEdge> iterator(){
+
+            final Iterator<Edge> itIn = graph.getGraphModel().getDirectedGraph().getInEdges(node).toCollection().iterator();
+            final Iterator<Edge> itOut = graph.getGraphModel().getDirectedGraph().getOutEdges(node).toCollection().iterator();
+
+            return new Iterator<GephiEdge>() {
+
+                Edge nextEdge = null;
+                @Override
+                public boolean hasNext() {
+
+                    if(labelHashSet.size() == 0){
+                        if(direction.equals("IN")){
+                            return itIn.hasNext();
+                        }
+                        else if(direction.equals("OUT")){
+                            return itOut.hasNext();
+                        }
+                    }
+
+                    if (nextEdge != null) {
+                        return true;
+                    }
+
+
+                    //Search for the next thing
+                    if (direction.equals("IN")) {
+                        while (itIn.hasNext()) {
+
+                            Edge temp = itIn.next();
+
+                            if (labelHashSet.contains(temp.getTypeLabel())) {
+                                nextEdge = temp;
+                                return true;
+                            }
+
+                        }
+
+
+                    } else {
+                        while (itOut.hasNext()) {
+
+                                Edge temp = itOut.next();
+
+                                    if (labelHashSet.contains(temp.getTypeLabel())) {
+                                        nextEdge = temp;
+                                        return true;
+                                    }
+                        }
+
+                    }
+                    return false;
+                }
+
+                @Override
+                public GephiEdge next() {
+
+
+
+                    if(labelHashSet.size() == 0){
+                        if(direction.equals("IN")){
+                            while(itIn.hasNext()) {
+                                return new GephiEdge(itIn.next(), graph);
+
+                            }
+                        }
+                        else if(direction.equals("OUT")){
+                            while(itOut.hasNext()) {
+                                return new GephiEdge(itOut.next(), graph);
+
+                            }
+                        }
+                    }
+
+                    if(nextEdge != null){
+                        Edge temp = nextEdge;
+                        nextEdge = null;
+                        return new GephiEdge(temp,graph);
+
+                    }
+
+                    //Search for the next thing
+                    if (direction.equals("IN")) {
+                        while (itIn.hasNext()) {
+                            Edge temp = itIn.next();
+                                    if (labelHashSet.contains(temp.getTypeLabel())) {
+                                        return new GephiEdge(temp, graph);
+
+                                    }
+                        }
+
+                    } else {
+                        while (itOut.hasNext()) {
+                            Edge temp = itOut.next();
+                                    if (labelHashSet.contains(temp.getTypeLabel())) {
+                                        return new GephiEdge(temp, graph);
+
+                                    }
+                        }
+
+                    }
+
+                    throw new NoSuchElementException();
+
+
+                }
+
+                @Override
+                public void remove() {
+                    if (direction.equals("IN")){
+                        itIn.remove();
+                    }
+                    else if(direction.equals("OUT")){
+                        itOut.remove();
+                    }
+                }
+            };
+        }
+
+
+    }
+
+
+}
---
+++ src/test/java/org/lab41/bptutorial/TinkerTestSuite.java
@@ -0,0 +1,103 @@
+package org.lab41.bptutorial;
+
+//import com.tinkerpop.blueprints.impls.GraphTest;
+import org.lab41.bptutorial.TestSuite;
+import org.lab41.bptutorial.VertexTestSuite;
+import org.lab41.bptutorial.EdgeTestSuite;
+import org.lab41.bptutorial.*;
+import org.lab41.bptutorial.util.io.graphml.GraphMLReaderTestSuite;
+import org.lab41.bptutorial.util.io.graphson.GraphSONReaderTestSuite;
+import com.tinkerpop.blueprints.*;
+import org.lab41.bptutorial.impls.GraphTest;
+
+import java.lang.reflect.Method;
+import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
+import org.gephi.graph.store.GraphFactoryImpl;
+import org.gephi.graph.store.GraphModelImpl;
+import org.gephi.graph.store.GraphStore;
+//import com.tinkerpop.blueprints.util.io.gml.GMLReaderTestSuite;
+//import com.tinkerpop.blueprints.util.io.graphml.GraphMLReaderTestSuite;
+//import com.tinkerpop.blueprints.util.io.graphson.GraphSONReaderTestSuite;
+import org.lab41.bptutorial.util.io.gml.GMLReaderTestSuite;
+/**
+ * Created by aganesh on 7/24/14.
+ */
+public class TinkerTestSuite extends GraphTest {
+    public void testVertexTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new VertexTestSuite(this));
+        printTestPerformance("VertexTestSuite", this.stopWatch());
+    }
+
+    public void testEdgeTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new EdgeTestSuite(this));
+        printTestPerformance("EdgeTestSuite", this.stopWatch());
+    }
+
+    public void testGraphTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GraphTestSuite(this));
+        printTestPerformance("GraphTestSuite", this.stopWatch());
+    }
+
+    /*public void testKeyIndexableGraphTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new KeyIndexableGraphTestSuite(this));
+        printTestPerformance("KeyIndexableGraphTestSuite", this.stopWatch());
+    }*/
+
+    /*public void testIndexableGraphTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new IndexableGraphTestSuite(this));
+        printTestPerformance("IndexableGraphTestSuite", this.stopWatch());
+    }*/
+
+    /*public void testIndexTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new IndexTestSuite(this));
+        printTestPerformance("IndexTestSuite", this.stopWatch());
+    }*/
+
+    /*public void testGraphMLReaderTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GraphMLReaderTestSuite(this));
+        printTestPerformance("GraphMLReaderTestSuite", this.stopWatch());
+    }*/
+
+    /*public void testGMLReaderTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GMLReaderTestSuite(this));
+        printTestPerformance("GMLReaderTestSuite", this.stopWatch());
+    }*/
+
+    /*public void testGraphSONReaderTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GraphSONReaderTestSuite(this));
+        printTestPerformance("GraphSONReaderTestSuite", this.stopWatch());
+    }*/
+
+    public Graph generateGraph() {
+        GraphModelImpl gl = new GraphModelImpl();
+        //GraphStore gp = new GraphStore(gl);
+        //GraphFactoryImpl gf = new GraphFactoryImpl(gp);
+        GephiGraph graph = new GephiGraph(gl);
+        return graph;
+    }
+
+    public Graph generateGraph(String str){
+        return generateGraph();
+    }
+
+    public void doTestSuite(final TestSuite testSuite) throws Exception {
+        String doTest = System.getProperty("testGephiGraph");
+        if (doTest == null || doTest.equals("true")) {
+            for (Method method : testSuite.getClass().getDeclaredMethods()) {
+                if (method.getName().startsWith("test")) {
+                    System.out.println("Testing " + method.getName() + "...");
+                    method.invoke(testSuite);
+                }
+            }
+        }
+    }
+}
